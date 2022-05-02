190b0cc2d7757fe574d821040bbc7f7079657ca3
alefbispo <Blacksabbath21@>
2022-05-01 21:10:34 -0300

    titulo

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,9 +18,25 @@ class _QuizAppState extends State<QuizApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('create'),
+          title: const Center(
+            child: Text('Quiz App'),
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.all(10),
+              child: const Text(
+                'titulo',
+                style: TextStyle(fontSize: 28),
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
         ),
       ),
     );
